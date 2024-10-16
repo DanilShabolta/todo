6cf060f9c8060b4b2eaee03fb0d2fe6594156d19
danilshabolta <danilshabolta@gmail.com>
2024-10-16 19:22:08 +0700

    fix: fixed add button

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
     const taskTitleInput = document.getElementById('task-title');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
-    const addButtonImage = document.querySelector('.add-button-container img');
+    const addButtonImage = document.querySelector('.add-button-container');
     
     let noTasksMessage = document.createElement('div');
     noTasksMessage.id = 'no-tasks-message';
